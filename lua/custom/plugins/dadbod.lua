ba4b40d6d3240466e47ccb8038d43b6b6000456b
Timo Myllymäki <timo.myllymaki@pinja.com>
2025-02-18 08:06:26 +0200

    updates

---
+++ lua/custom/plugins/dadbod.lua
@@ -0,0 +1,19 @@
+return {
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_winwidth = 40
+    vim.g.db_ui_use_nvim_notify = 1
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+}
