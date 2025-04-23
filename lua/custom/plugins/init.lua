6cfd140b3aeef60e95c515abaad56bccd82f8b7b
Timo Myllymäki <timo.myllymaki@pinja.com>
2025-04-23 08:19:55 +0300

    something

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,6 +9,43 @@ return {
     enabled = true,
   },
   {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+  },
+  {
     'seblyng/roslyn.nvim',
     ft = { 'cs', 'razor' },
     dependencies = {
@@ -25,6 +62,7 @@ return {
         -- By loading as a dependencies, we ensure that we are available to set
         -- the handlers for roslyn
         'tris203/rzls.nvim',
+        branch = 'pullDiags',
         config = function()
           ---@diagnostic disable-next-line: missing-fields
           require('rzls').setup {}
@@ -160,6 +198,9 @@ return {
             end,
             build = function()
               return string.format('dotnet build %s %s', path, args)
+            end,
+            watch = function()
+              return string.format('dotnet watch %s %s', path, args)
             end,
           }
 
