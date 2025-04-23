0ae186ec84e6f95753e986ca8d3df46df2a0cb3c
Timo Myllymäki <timo.myllymaki@pinja.com>
2025-04-23 08:21:18 +0300

    merge fix

--- init.lua
+++ init.lua
@@ -164,7 +164,7 @@ vim.o.scrolloff = 10
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.o.confirm = true
+vim.opt.confirm = true
 vim.g.matchparen_timeout = 2
 vim.g.matchparen_insert_timeout = 2
 
