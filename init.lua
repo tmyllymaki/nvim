0c52230d5353e942701bc03c5a5a8d4b8d7c968b
Timo Myllymäki <tmyllymaki@fastmail.com>
2025-05-29 20:53:00 +0300

    Updates

--- init.lua
+++ init.lua
@@ -995,6 +995,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  { import = 'custom.lsp' },
   { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
