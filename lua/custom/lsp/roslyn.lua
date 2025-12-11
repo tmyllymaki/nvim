324df1eec23189017721397d421faa47005a50ac
Timo Myllymäki <timo.myllymaki@pinja.com>
2025-12-11 11:37:08 +0200

    feat: new cohost roslyn razor support

--- lua/custom/lsp/roslyn.lua
+++ lua/custom/lsp/roslyn.lua
@@ -1,6 +1,5 @@
 return {
   'seblyng/roslyn.nvim',
-  ft = { 'cs', 'razor' },
   dependencies = {
     {
       'mason-org/mason.nvim',
@@ -11,31 +10,13 @@ return {
         },
       },
     },
-    {
-      'tris203/rzls.nvim',
-      config = true,
-    },
   },
   opts = {
     broad_search = true,
     filewatching = 'roslyn',
   },
   config = function()
-    local rzls_path = vim.fn.expand '$MASON/packages/rzls/libexec'
-    local cmd = {
-      'roslyn',
-      '--stdio',
-      '--logLevel=Information',
-      '--extensionLogDirectory=' .. vim.fs.dirname(vim.lsp.get_log_path()),
-      '--razorSourceGenerator=' .. vim.fs.joinpath(rzls_path, 'Microsoft.CodeAnalysis.Razor.Compiler.dll'),
-      '--razorDesignTimePath=' .. vim.fs.joinpath(rzls_path, 'Targets', 'Microsoft.NET.Sdk.Razor.DesignTime.targets'),
-      '--extension',
-      vim.fs.joinpath(rzls_path, 'RazorExtension', 'Microsoft.VisualStudioCode.RazorExtension.dll'),
-    }
-
     vim.lsp.config('roslyn', {
-      cmd = cmd,
-      handlers = require 'rzls.roslyn_handlers',
       settings = {
         ['csharp|inlay_hints'] = {
           csharp_enable_inlay_hints_for_implicit_object_creation = true,
@@ -56,8 +37,12 @@ return {
           dotnet_enable_references_code_lens = true,
         },
         ['csharp|completion'] = {
+          dotnet_provide_regex_completions = true,
           dotnet_show_name_completion_suggestions = true,
           dotnet_show_completion_items_from_unimported_namespaces = true,
+        },
+        ['csharp|formatting'] = {
+          dotnet_organize_imports_on_format = true,
         },
         ['csharp|background_analysis'] = {
           background_analysis = {
@@ -68,14 +53,5 @@ return {
       },
     })
     vim.lsp.enable 'roslyn'
-  end,
-  init = function()
-    -- We add the Razor file types before the plugin loads.
-    vim.filetype.add {
-      extension = {
-        razor = 'razor',
-        cshtml = 'razor',
-      },
-    }
   end,
 }
